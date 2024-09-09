0854869ee79dd0bd8a1c34c8db92650040c37d15
lukaniininen <luka.niinunen@gmail.com>
2024-09-09 12:18:19 +0300

    proto map
    eknttäää

--- Assets/Scenes/testit/Scripts/Door.cs
+++ Assets/Scenes/testit/Scripts/Door.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Animator anim;
-    public Transform palyer;
+    public Transform player;
     public Transform door;
  
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(palyer.position, door.position);
+        float distance = Vector3.Distance(player.position, door.position);
 
         if (distance <= 25)
         {
