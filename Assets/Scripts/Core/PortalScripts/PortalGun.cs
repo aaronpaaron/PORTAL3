a33efdbed15718dca97721222fb4cba5a25f86ca
Tese <gr289448@gradia.fi>
2024-09-11 13:15:45 +0300

    Portaali shit

--- Assets/Scripts/Core/PortalScripts/PortalGun.cs
+++ Assets/Scripts/Core/PortalScripts/PortalGun.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PortalShooter : MonoBehaviour
+public class PortalGun : MonoBehaviour
 {
     public GameObject portalA; // Viittaa Portaali A:han
     public GameObject portalB; // Viittaa Portaali B:hen
@@ -32,25 +32,41 @@ public class PortalShooter : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             Vector3 hitPoint = hit.point;
-            Quaternion hitRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            Vector3 hitNormal = hit.normal;
 
+            // Aseta portaalin sijainti
             if (isPortalAActive)
             {
-                // Aseta Portaali B näkyviin ja aseta sen sijainti raycast-kohtaan
                 portalB.SetActive(true);
                 portalB.transform.position = hitPoint;
-                portalB.transform.rotation = hitRotation;
+                portalB.transform.rotation = CalculatePortalRotation(hitNormal);
             }
             else
             {
-                // Aseta Portaali A näkyviin ja aseta sen sijainti raycast-kohtaan
                 portalA.SetActive(true);
                 portalA.transform.position = hitPoint;
-                portalA.transform.rotation = hitRotation;
+                portalA.transform.rotation = CalculatePortalRotation(hitNormal);
             }
 
             // Vaihda aktiivista portaalitilaa
             isPortalAActive = !isPortalAActive;
         }
+    }
+
+    Quaternion CalculatePortalRotation(Vector3 hitNormal)
+    {
+        // Laske portaalin y-akseli ylöspäin suhteessa osumapintaan
+        Vector3 up = Vector3.up;
+        // Käytetään cameraa määrittämään, mihin suuntaan portaalin etupään pitäisi osoittaa
+        Vector3 forward = Camera.main.transform.forward;
+
+        // Laske rotaatio niin, että portaalin y-akseli on ylöspäin
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hitNormal);
+
+        // Kierrä portaalin etupää kameran suuntaan
+        Vector3 desiredForward = rotation * forward;
+        rotation = Quaternion.LookRotation(desiredForward, up);
+
+        return rotation;
     }
 }
