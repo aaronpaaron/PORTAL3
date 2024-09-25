f17ae406cc4a778cc6f8afdca89263705d20e12b
Aaron Rahm <gr289430@gradia.fi>
2024-09-25 10:37:19 +0300

    notet toimii

---
+++ Assets/Note.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public Sprite noteSprite;  // Image that represents the note
+
+    // You can expand this to include more properties, such as note text, sound, etc.
+    // public string noteText;  // If you want to show text instead of or alongside the image
+}
